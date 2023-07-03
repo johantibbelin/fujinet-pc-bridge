937a8932d89b615328ff663656b425ec181624d2
Johan Tibbelin <johan.roos@tibbelin.com>
2023-07-03 11:18:14 +0200

    More netSIO defs

--- netsiodefs.h
+++ netsiodefs.h
@@ -38,4 +38,30 @@ char buf[UDP_BUFFER_SIZE];
 #define NETSIO_ALIVE_RESPONSE   0xC5
 #define NETSIO_WARM_RESET       0xFE
 #define NETSIO_COLD_RESET       0xFF
+
+// events to manage device connection (connect, ping, alive) >= 0xC0
+#define NETSIO_CONN_MGMT        0xC0
+
+// NETSIO_SYNC_RESPONSE types
+#define NETSIO_EMPTY_SYNC       0x00
+#define NETSIO_ACK_SYNC         0x01
+
+// Altirra specific (not used at the moment)
+#define ATDEV_READY             0x100
+#define ATDEV_TRANSMIT_BUFFER   0x101
+#define ATDEV_DEBUG_MESSAGE     0x102
+#define ATDEV_EMPTY_SYNC        -1 # any value less than zero
+
+
+// local TCP port for Altirra custom device communication
+#define NETSIO_ATDEV_PORT   9996
+// UDP port NetSIO is accepting messages from peripherals
+#define NETSIO_PORT         9997
+
+/* client expiration period in seconds
+   if NetSIO HUB will not receive alive message (NETSIO_ALIVE_REQUEST) from device the device
+   connection is being considered as expired and the device is disconnected from the HUB
+*/
+#define ALIVE_EXPIRATION 30.0f
+
 #endif /* NETSIODEFS_H */
