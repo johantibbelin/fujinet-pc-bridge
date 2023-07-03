21a03b2de770e16eafe22a088ceca134ffe02f59
Johan Tibbelin <johan.roos@tibbelin.com>
2023-07-03 11:01:56 +0200

    Start netsiodefs.h

--- netsiodefs.h
+++ netsiodefs.h
@@ -1,4 +1,15 @@
 #ifndef NETSIODEFS_H
 #define NETSIODEFS_H
 
+/* UDP Defines */
+#define UDP_PORT 5000
+#define UDP_BUFFER_SIZE 1024
+
+struct addrinfo hints;
+struct addrinfo *result, *rp;
+int sfd, s, j;
+size_t len;
+ssize_t nread;
+char buf[UDP_BUFFER_SIZE];
+
 #endif /* NETSIODEFS_H */
