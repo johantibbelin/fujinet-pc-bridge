cacd535ae11feaff12e1edf8d3cd417abfc095d4
Johan Tibbelin <johan.roos@tibbelin.com>
2023-07-19 17:26:39 +0200

    Update fujinetbridge.h

--- fujinetbridge.h
+++ fujinetbridge.h
@@ -1,3 +1,14 @@
+/**
+ * @file fujinetbridge.h
+ * @author Johan Tibbelin (sjfroos) (johan.roos@tibbelin.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-07-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 /* Global defines */
 #ifndef FUJINETBRIDGE_H
 #define FUJINETBRIDGE_H
